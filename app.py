0ece858c25213ad22506b99b893eac78794155ed
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-10-02 22:49:56 +0530

    error resolved

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain.chains import SequentialChain
 from dotenv import load_dotenv
 load_dotenv()
 
-os.getenv("HUGGINGFACE_API_TOKEN")
+os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 st.title("First streamlit app")
 display_text=st.text_input("Type a Topic/Person to get the information")
